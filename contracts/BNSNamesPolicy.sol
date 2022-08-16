ceeea42680adc93ac23706db4909b9c6c660e67c
Arkadiy A.Davydov <davydov@blockwit.io>
2022-08-16 21:19:57 +0300

    refactor(contracts): move StringUtils to lib

--- contracts/BNSNamesPolicy.sol
+++ contracts/BNSNamesPolicy.sol
@@ -3,7 +3,7 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/access/AccessControl.sol";
-import "./StringUtils.sol";
+import "./lib/StringUtils.sol";
 
 contract BNSNamesPolicy is AccessControl {
 

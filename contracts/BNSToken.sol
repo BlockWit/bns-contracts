5bef1cb91c82dc695146837551cbee89d2e790d7
Arkadiy A.Davydov <davydov@blockwit.io>
2022-08-21 01:37:03 +0300

    fix(contracts/BNSToken): fix constructor, add setAsset and removeAsset methods

--- contracts/BNSToken.sol
+++ contracts/BNSToken.sol
@@ -2,10 +2,20 @@
 
 pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts/access/AccessControl.sol";
 import "./DividendPayingToken.sol";
 
-contract BNSToken is DividendPayingToken {
-    constructor(string memory name, string memory symbol, address initialAccount, uint256 initialBalance) payable ERC20(name, symbol) {
+contract BNSToken is DividendPayingToken, AccessControl {
+    constructor(address initialAccount, uint256 initialBalance) payable ERC20("BNSToken", "BNST") {
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
         _mint(initialAccount, initialBalance);
+    }
+
+    function setAsset(address key, string memory assetTicker, Assets.AssetType assetType) external onlyRole(DEFAULT_ADMIN_ROLE) returns (bool) {
+        return _setAsset(key, assetTicker, assetType);
+    }
+
+    function removeAsset(address key) onlyRole(DEFAULT_ADMIN_ROLE) external returns (bool) {
+        return _removeAsset(key);
     }
 }

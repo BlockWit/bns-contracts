19c2d5436ece0eaa4a85cf238dc7ae868a9601ba
Alexander Strakh <cromlehg@gmail.com>
2022-08-23 22:41:44 +0300

    ORG-49: Premium domains funcs has been added

--- contracts/BNSMarketPricePolicy.sol
+++ contracts/BNSMarketPricePolicy.sol
@@ -10,11 +10,25 @@ contract BNSMarketPricePolicy is Ownable {
     using StringUtils for string;
 
     mapping (uint => uint) public pricePerNameLength;
+
+    mapping (bytes32 => uint) public premiumDomainPrices;
+
     uint public defaultPrice;
 
+    function getPriceForPremiumDomain(string memory domainName) public view returns(uint) {
+        bytes32 hash = keccak256(abi.encodePacked(domainName));
+        uint price = premiumDomainPrices[keccak256(abi.encodePacked(domainName))];
+        require(price > 0, "Domain not in premium list");
+        return price;
+    }
+
     function getPrice(string memory domainName, string memory refererDomainName, address assetKey) public view returns(uint) {
-        uint price = pricePerNameLength[domainName.length()];
-        if(price == 0) price = defaultPrice;
+        bytes32 hash = keccak256(abi.encodePacked(domainName));
+        uint price = premiumDomainPrices[keccak256(abi.encodePacked(domainName))];
+        if(price == 0) {
+            uint price = pricePerNameLength[domainName.length()];
+            if(price == 0) price = defaultPrice;
+        }
         return price;
     }
 
@@ -24,6 +38,29 @@ contract BNSMarketPricePolicy is Ownable {
 
     function setPrice(uint size, uint price) public onlyOwner {
         pricePerNameLength[size] = price;
+    }
+
+    /**
+    *
+    * Domain names should performed before fill
+    *
+    **/
+    function unsafeSetPremiumDomainPrices(string[] memory domainNames, uint[] memory prices) public onlyOwner {
+        require(domainNames.length == prices.length, "Count of domain names and prices must be equals!");
+        for(uint i = 0; i < domainNames.length; i++) {
+           bytes32 hash = keccak256(abi.encodePacked(domainNames[i]));
+           premiumDomainPrices[hash] = prices[i];
+        }
+    }
+
+    /**
+    *
+    * Domain names should performed before fill
+    *
+    **/
+    function unsafeSetPremiumDomainPrice(string memory domainName, uint price) public onlyOwner {
+       bytes32 hash = keccak256(abi.encodePacked(domainName));
+       premiumDomainPrices[hash] = price;
     }
 
     function setPrices(uint newDefaultPrice, uint[] memory sizes, uint[] memory prices) public onlyOwner {

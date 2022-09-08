6f1db549790e9603b9857cb4dcd067f89fded4cb
Arkadiy A.Davydov <arkadiy.a.davydov@gmail.com>
2022-09-08 21:23:27 +0300

    chore(BNSMarketPricePolicy): set special price for domain names with the first character from a special range

--- contracts/BNSMarketPricePolicy.sol
+++ contracts/BNSMarketPricePolicy.sol
@@ -5,18 +5,25 @@ pragma solidity ^0.8.14;
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "./lib/Assets.sol";
 import "./lib/StringUtils.sol";
+import "./lib/UTF8Utils.sol";
 import "./DiscountCalculator.sol";
 import "./RecoverableFunds.sol";
 
 contract BNSMarketPricePolicy is Ownable, DiscountCalculator, RecoverableFunds {
 
+    struct UTF8Range {
+        bytes4 first;
+        bytes4 last;
+    }
+
     using StringUtils for string;
 
     mapping (uint => uint) public pricePerNameLength;
-
+    mapping (uint => uint) public pricePerNameLengthForSymbolsWithinRange;
     mapping (bytes32 => uint) public premiumDomainPrices;
-
+    UTF8Range[] public utf8ranges;
     uint public defaultPrice;
+    uint public defaultPriceForSymbolsWithinRange;
 
     function getPriceForPremiumDomain(string memory domainName) external view returns(uint) {
         uint price = premiumDomainPrices[keccak256(abi.encodePacked(domainName))];
@@ -25,10 +32,18 @@ contract BNSMarketPricePolicy is Ownable, DiscountCalculator, RecoverableFunds {
     }
 
     function getPrice(string memory domainName, Assets.Key assetKey, bool hasReferer) external view returns(uint) {
-        uint price = premiumDomainPrices[keccak256(abi.encodePacked(domainName))];
-        if(price == 0) {
+        uint256 price = premiumDomainPrices[keccak256(abi.encodePacked(domainName))];
+        if (price == 0 && isWithinRange(domainName)) {
+            price = pricePerNameLengthForSymbolsWithinRange[domainName.length()];
+            if (price == 0) {
+                price = defaultPriceForSymbolsWithinRange;
+            }
+        }
+        if (price == 0) {
             price = pricePerNameLength[domainName.length()];
-            if(price == 0) price = defaultPrice;
+        }
+        if (price == 0) {
+            price = defaultPrice;
         }
         if (hasReferer) {
             price = price - calculateDiscount(price, block.timestamp);
@@ -40,6 +55,10 @@ contract BNSMarketPricePolicy is Ownable, DiscountCalculator, RecoverableFunds {
         defaultPrice = price;
     }
 
+    function setDefaultPriceForSymbolsWithinRange(uint price) public onlyOwner {
+        defaultPriceForSymbolsWithinRange = price;
+    }
+
     function setDiscount(Discount[] calldata discounts) external onlyOwner {
         _setDiscount(discounts);
     }
@@ -48,10 +67,27 @@ contract BNSMarketPricePolicy is Ownable, DiscountCalculator, RecoverableFunds {
         pricePerNameLength[size] = price;
     }
 
+    function setPriceForSymbolsWithinRange(uint size, uint price) public onlyOwner {
+        pricePerNameLengthForSymbolsWithinRange[size] = price;
+    }
+
+    function addUTF8Range(bytes4 first, bytes4 last) external onlyOwner {
+        utf8ranges.push(UTF8Range(first, last));
+    }
+
+    function removeUTF8Range(uint256 index) external onlyOwner {
+        for (uint256 i = index; i < utf8ranges.length - 1; i++) {
+            utf8ranges[i] = utf8ranges[i + 1];
+        }
+        utf8ranges.pop();
+    }
+
+    function setUTF8Range(bytes4 first, bytes4 last, uint256 index) external onlyOwner {
+        utf8ranges[index] = UTF8Range(first, last);
+    }
+
     /**
-    *
-    * Domain names should performed before fill
-    *
+    * Domain names must be sanitized prior to calling this method
     **/
     function unsafeSetPremiumDomainPrices(string[] memory domainNames, uint[] memory prices) public onlyOwner {
         require(domainNames.length == prices.length, "Count of domain names and prices must be equals!");
@@ -61,9 +97,7 @@ contract BNSMarketPricePolicy is Ownable, DiscountCalculator, RecoverableFunds {
     }
 
     /**
-    *
-    * Domain names should performed before fill
-    *
+    * Domain name must be sanitized prior to calling this method
     **/
     function unsafeSetPremiumDomainPrice(string memory domainName, uint price) public onlyOwner {
        premiumDomainPrices[keccak256(abi.encodePacked(domainName))] = price;
@@ -83,6 +117,15 @@ contract BNSMarketPricePolicy is Ownable, DiscountCalculator, RecoverableFunds {
 
     function retrieveETH(address payable recipient) external onlyOwner {
         _retrieveETH(recipient);
+    }
+
+    function isWithinRange(string memory domainName) public view returns (bool) {
+        bytes4 firstCharCode = UTF8Utils.getCharCodeAt(domainName, 0);
+        for (uint256 i; i < utf8ranges.length; i++) {
+            UTF8Range memory range = utf8ranges[i];
+            if (firstCharCode >= range.first && firstCharCode <= range.last) return true;
+        }
+        return false;
     }
 
 }
--- contracts/lib/UTF8Utils.sol
+++ contracts/lib/UTF8Utils.sol
@@ -16,27 +16,29 @@ library UTF8Utils {
         }
     }
 
-    function getFirstCharCodepoint(string memory _self) external pure returns (uint256) {
+    function getCharCodeAt(string memory _self, uint256 position) internal pure returns (bytes4) {
         bytes memory _bytes = bytes(_self);
-        bytes1 firstByte = _bytes[0];
+        uint256 i = 0;
+        uint256 size = 0;
+        while (size < position) {
+            i += _getCharSize(_bytes[i]);
+            size++;
+        }
+        bytes1 firstByte = _bytes[i];
         if (firstByte >> 7 == bytes1(0)) {
-            // this is a single byte UTF-8 char
-            return uint8(firstByte);
+            return bytes4(firstByte) >> 24;
         } else if (firstByte >> 5 == bytes1(uint8(0x6))) {
-            // two byte UTF-8 char
-            return uint16((firstByte & 0x1f) >> 2 | bytes2(_bytes[1] & 0x3f) >> 8);
+            return bytes4(firstByte) >> 16 | bytes4(_bytes[i + 1]) >> 24;
         } else if (firstByte >> 4 == bytes1(uint8(0xe))) {
-            // three byte UTF-8 char
-            return uint16((firstByte & 0x1f) << 4 | bytes2(_bytes[1] & 0x3f) >> 2 | bytes2(_bytes[2] & 0x3f) >> 8);
+            return bytes4(firstByte) >> 8 | bytes4(_bytes[i + 1]) >> 16 | bytes4(_bytes[i + 2]) >> 24;
         } else if (firstByte >> 3 == bytes1(uint8(0x1e))) {
-            // four byte UTF-8 char
-            return uint24((firstByte & 0x07) << 2 | bytes2(_bytes[1] & 0x3f) >> 4 | bytes3(_bytes[2] & 0x3f) >> 10 | bytes3(_bytes[3] & 0x3f) >> 16);
+            return bytes4(firstByte) | bytes4(_bytes[i + 1]) >> 8 | bytes4(_bytes[i + 2]) >> 16 | bytes4(_bytes[i + 3]) >> 24;
         } else {
             revert("UTF8Utils: unsupported character");
         }
     }
 
-    function _getCharSize(bytes1 _byte1) internal pure returns (uint8 size) {
+    function _getCharSize(bytes1 _byte1) private pure returns (uint8 size) {
         if (_byte1 >> 7 == 0) size = 1;
         else if (_byte1 >> 5 == bytes1(uint8(0x6))) size = 2;
         else if (_byte1 >> 4 == bytes1(uint8(0xE))) size = 3;

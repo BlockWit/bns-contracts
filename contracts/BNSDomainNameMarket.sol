68d28148f6fd035a11f3a5cb4925c276440d63ba
Artem <artknutov@gmail.com>
2022-09-24 20:43:13 +0300

    refactor(project): add buy for backend, clean contracts and tests

--- contracts/BNSDomainNameMarket.sol
+++ contracts/BNSDomainNameMarket.sol
@@ -8,29 +8,17 @@ import "@openzeppelin/contracts/access/AccessControl.sol";
 import "./interfaces/IDividendManager.sol";
 import "./lib/Assets.sol";
 import "./BNSNFT.sol";
-import "./BNSMarketPricePolicy.sol";
-import "./BNSNamesPolicy.sol";
 import "./AssetHandler.sol";
 
 contract BNSDomainNameMarket is Pausable, AccessControl, AssetHandler, RecoverableFunds {
 
-    BNSMarketPricePolicy public pricePolicy;
-    BNSNamesPolicy public namesPolicy;
     BNSNFT public bnsnft;
     IDividendManager public dividendManager;
-    uint256 public refererBonusNumerator = 10;
-    uint256 public refererBonusDenominator = 100;
-
-    struct DomainInfo {
-        string name;
-        uint256 fullPrice;
-        uint256 discountedPrice;
-        bool isAvailable;
-        address owner;
-    }
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
 
     constructor() {
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _grantRole(MINTER_ROLE, msg.sender);
     }
 
     function setBNSNFT(address newBnsnft) public onlyRole(DEFAULT_ADMIN_ROLE) {
@@ -41,174 +29,28 @@ contract BNSDomainNameMarket is Pausable, AccessControl, AssetHandler, Recoverab
         dividendManager = IDividendManager(newDividendManager);
     }
 
-    function setPricePolicy(address newPricePolicy) external onlyRole(DEFAULT_ADMIN_ROLE) {
-        pricePolicy = BNSMarketPricePolicy(newPricePolicy);
-    }
-
-    function setNamesPolicy(address newNamesPolicy) external onlyRole(DEFAULT_ADMIN_ROLE) {
-        namesPolicy = BNSNamesPolicy(newNamesPolicy);
-    }
-
     function setAsset(Assets.Key key, string memory assetTicker, Assets.AssetType assetType) external onlyRole(DEFAULT_ADMIN_ROLE) returns (bool) {
         return _setAsset(key, assetTicker, assetType);
-    }
-
-    function setRefererBonus(uint256 numerator, uint256 denominator) external onlyRole(DEFAULT_ADMIN_ROLE) {
-        refererBonusNumerator = numerator;
-        refererBonusDenominator = denominator;
     }
 
     function removeAsset(Assets.Key key) external onlyRole(DEFAULT_ADMIN_ROLE) returns (bool) {
         return _removeAsset(key);
     }
 
-    function getPriceWithoutReferer(string memory domainName, Assets.Key assetKey) public view returns(uint) {
-        return getPrice(domainName, "", assetKey);
-    }
-
-    function getPrice(string memory domainName, string memory refererDomainName, Assets.Key assetKey) public view returns(uint) {
-        require(!bnsnft.isDomainNameExists(domainName), "Domain name already exists");
-
-        if (bytes(refererDomainName).length > 0) {
-            refererDomainName = namesPolicy.perform(refererDomainName);
-            require(bnsnft.isDomainNameExists(refererDomainName), "Referer domain name must exists");
-        }
-
-        domainName = namesPolicy.perform(domainName);
-        namesPolicy.check(domainName);
-        return pricePolicy.getPrice(domainName, assetKey, bytes(refererDomainName).length > 0);
-    }
-
-    //!!!!!!! NOT TESTED  !!!!!!!!
-    function getDomainInfo(string[] calldata domainNames, string memory refererDomainName, Assets.Key assetKey) external view returns (DomainInfo[] memory) {
-        DomainInfo[] memory domainsInfo = new DomainInfo[](domainNames.length);
-
-        if (bytes(refererDomainName).length > 0) {
-            refererDomainName = namesPolicy.perform(refererDomainName);
-            require(bnsnft.isDomainNameExists(refererDomainName), "Referer domain name must exists");
-        }
-
-        for(uint i = 0; i < domainNames.length; i++ ) {
-            DomainInfo memory domainInfo;
-            string memory domainName = domainNames[i];
-            domainName = namesPolicy.perform(domainName);
-            namesPolicy.check(domainName);
-
-            //set name
-            domainInfo.name = domainName;
-
-            //set isAvailable and owner
-            if (bnsnft.isDomainNameExists(domainName)) {
-                domainInfo.isAvailable = false;
-                domainInfo.owner = bnsnft.ownerOf(bnsnft.getTokenIdByDomainName(domainName));
-            } else {
-                domainInfo.isAvailable = true;
-            }
-
-            //set fullPrice and discountedPrice
-            if (bytes(refererDomainName).length > 0) {
-                domainInfo.fullPrice = pricePolicy.getPrice(domainName, assetKey, false);
-                domainInfo.discountedPrice = pricePolicy.getPrice(domainName, assetKey, true);
-            } else {
-                domainInfo.fullPrice = pricePolicy.getPrice(domainName, assetKey, false);
-                domainInfo.discountedPrice = domainInfo.fullPrice;
-            }
-
-            domainsInfo[i] = domainInfo;
-        }
-
-        return domainsInfo;
-    }
-
-
-    function buyWithoutReferer(string memory domainName, Assets.Key assetKey) external {
-        domainName = namesPolicy.perform(domainName);
-        namesPolicy.check(domainName);
-        require(!bnsnft.isDomainNameExists(domainName), "Domain name already exists");
-        uint256 price = pricePolicy.getPrice(domainName, assetKey, false);
-
+    function buy(string[] memory domainNames, uint price, address buyer, address referer, uint refererBonus, Assets.Key assetKey) whenNotPaused external onlyRole(MINTER_ROLE) {
         // charge payment
-        _transferAssetFrom(msg.sender, address(this), price, assetKey);
+        _transferAssetFrom(buyer, address(this), price, assetKey);
 
-        _approveAsset(address(dividendManager), price, assetKey);
-        dividendManager.distributeDividends(price, assetKey);
-
-        // mint NFT
-        bnsnft.safeMint(msg.sender, domainName);
-    }
-
-    function buy(string memory domainName, string memory refererDomainName, Assets.Key assetKey) whenNotPaused external {
-        uint refererTokenId;
-        address refererAddress;
-        bool hasReferer;
-        if (bytes(refererDomainName).length > 0) {
-            refererDomainName = namesPolicy.perform(refererDomainName);
-            refererTokenId = bnsnft.getTokenIdByDomainName(refererDomainName);
-            refererAddress = bnsnft.ownerOf(refererTokenId);
-            hasReferer = true;
-        }
-        // sanitize domain name and calculate price
-        domainName = namesPolicy.perform(domainName);
-        namesPolicy.check(domainName);
-        require(!bnsnft.isDomainNameExists(domainName), "Domain name already exists");
-        uint256 price = pricePolicy.getPrice(domainName, assetKey, hasReferer);
-
-        // charge payment
-        _transferAssetFrom(msg.sender, address(this), price, assetKey);
-
-        uint256 refererBonus;
-        uint256 dividends = price;
-        if (hasReferer) {
-            refererBonus = refererBonusDenominator > 0 ? price * refererBonusNumerator / refererBonusDenominator : 0;
-        }
+        uint dividends = price;
         if (refererBonus > 0) {
             dividends = dividends - refererBonus;
-            _transferAsset(refererAddress, refererBonus, assetKey);
-        }
-        _approveAsset(address(dividendManager), dividends, assetKey);
-        dividendManager.distributeDividends(price - refererBonus, assetKey);
-
-        // mint NFT
-        bnsnft.safeMint(msg.sender, domainName);
-    }
-
-    function buyBulk(string[] memory domainNames, string memory refererDomainName, Assets.Key assetKey) whenNotPaused external {
-        uint refererTokenId;
-        address refererAddress;
-        bool hasReferer;
-        if (bytes(refererDomainName).length > 0) {
-            refererDomainName = namesPolicy.perform(refererDomainName);
-            refererTokenId = bnsnft.getTokenIdByDomainName(refererDomainName);
-            refererAddress = bnsnft.ownerOf(refererTokenId);
-            hasReferer = true;
-        }
-
-        // sanitize domain names and calculate overall price
-        uint256 price = 0;
-        for(uint i = 0; i < domainNames.length; i++ ) {
-            domainNames[i] = namesPolicy.perform(domainNames[i]);
-            namesPolicy.check(domainNames[i]);
-            require(!bnsnft.isDomainNameExists(domainNames[i]), "Domain name already exists");
-            price = price + pricePolicy.getPrice(domainNames[i], assetKey, hasReferer);
-        }
-
-        // charge payment
-        _transferAssetFrom(msg.sender, address(this), price, assetKey);
-
-        uint256 refererBonus;
-        uint256 dividends = price;
-        if (hasReferer) {
-            refererBonus = refererBonusDenominator > 0 ? price * refererBonusNumerator / refererBonusDenominator : 0;
-        }
-        if (refererBonus > 0) {
-            dividends = dividends - refererBonus;
-            _transferAsset(refererAddress, refererBonus, assetKey);
+            _transferAsset(referer, refererBonus, assetKey);
         }
         _approveAsset(address(dividendManager), dividends, assetKey);
         dividendManager.distributeDividends(price - refererBonus, assetKey);
 
         // mint all NFT
-        bnsnft.safeBatchMint(msg.sender, domainNames);
+        bnsnft.safeBatchMint(buyer, domainNames);
     }
 
     function retrieveTokens(address recipient, address tokenAddress) external onlyRole(DEFAULT_ADMIN_ROLE) {
--- test/bnsdomainnamemarket.test.js
+++ test/bnsdomainnamemarket.test.js
@@ -4,8 +4,6 @@ const { assert, expect } = require('chai');
 const { getEvents } = require('./util');
 
 const BNSDomainNameMarket = contract.fromArtifact('BNSDomainNameMarket');
-const BNSMarketPricePolicy = contract.fromArtifact('BNSMarketPricePolicy');
-const BNSNamesPolicy = contract.fromArtifact('BNSNamesPolicy');
 const BNSNFT = contract.fromArtifact('BNSNFT');
 const ERC20Mock = contract.fromArtifact('ERC20Mock');
 const InvestNFT = contract.fromArtifact('InvestNFT');
@@ -13,20 +11,15 @@ const InvestNFTMarket = contract.fromArtifact('InvestNFTMarket');
 const InvestNFTMarketPricePolicy = contract.fromArtifact('InvestNFTMarketPricePolicy');
 const DividendManager = contract.fromArtifact('DividendManager');
 
-const SIZES = [1,2,3,4,5,6,7,8];
 const PRICE = ether('0.000000000000000001');
-const PRICES_USDT = [300000,250000,200000,100000,50000,10000,1000,100];
-const BASE_PRICE_USDT = 100;
 const domainNames = ['a', 'ab', 'abc', 'abcd', 'abcde'];
 
-const [ deployer, user, holder1, holder2, holder3 ] = accounts;
+const [ deployer, user, holder1, holder2, holder3, seller, referer ] = accounts;
 
 describe('BNSDomainNameMarket', function () {
   this.timeout(0);
   let market;
-  let names;
   let nft;
-  let pricing;
   let dividend;
   let investnft;
   let investnftmarket;
@@ -34,15 +27,13 @@ describe('BNSDomainNameMarket', function () {
   let tokens = { usdt: { id: 1, contract: undefined, key: 12345 }, busd: { id: 2, contract: undefined, key: 23456 }};
 
   beforeEach(async function () {
-    [ investnft, investnftmarket, investpricing, market, names, nft, dividend, pricing, tokens.busd.contract, tokens.usdt.contract ] = await Promise.all([
+    [ investnft, investnftmarket, investpricing, market, nft, dividend, tokens.busd.contract, tokens.usdt.contract ] = await Promise.all([
       InvestNFT.new({from: deployer}),
       InvestNFTMarket.new({from: deployer}),
       InvestNFTMarketPricePolicy.new({from: deployer}),
       BNSDomainNameMarket.new({ from: deployer }),
-      BNSNamesPolicy.new({ from: deployer }),
       BNSNFT.new({ from: deployer }),
       DividendManager.new({ from: deployer }),
-      BNSMarketPricePolicy.new({ from: deployer }),
       ERC20Mock.new('BUSD Mock Token', 'BUSD', deployer, ether('10000000'), { from: deployer }),
       ERC20Mock.new('USDT Mock Token', 'USDT', deployer, ether('10000000'), { from: deployer }),
     ]);
@@ -80,34 +71,6 @@ describe('BNSDomainNameMarket', function () {
     });
   });
 
-  describe('setPricePolicy', function () {
-    context('when called by admin', function () {
-      it('should change pricePolicy', async function () {
-        await market.setPricePolicy(pricing.address, {from : deployer});
-        expect(await market.pricePolicy()).to.be.equal(pricing.address);
-      });
-    });
-    context('when called not by admin', function () {
-      it('revert', async function () {
-        await expectRevert.unspecified(market.setPricePolicy(pricing.address, {from : user}));
-      });
-    });
-  });
-
-  describe('setNamesPolicy', function () {
-    context('when called by admin', function () {
-      it('should change namesPolicy', async function () {
-        await market.setNamesPolicy(names.address, {from : deployer});
-        expect(await market.namesPolicy()).to.be.equal(names.address);
-      });
-    });
-    context('when called not by admin', function () {
-      it('revert', async function () {
-        await expectRevert.unspecified(market.setNamesPolicy(names.address, {from : user}));
-      });
-    });
-  });
-
   describe('setAsset', function () {
     context('when called by admin', function () {
       it('should add specified asset to assets.map', async function () {
@@ -124,21 +87,6 @@ describe('BNSDomainNameMarket', function () {
     });
   });
 
-    describe('setRefererBonus', function () {
-        context('when called by admin', function () {
-            it('should change numerator and denominator', async function () {
-                await market.setRefererBonus(20, 200, {from : deployer});
-                expect(await market.refererBonusNumerator()).to.be.bignumber.equal('20');
-                expect(await market.refererBonusDenominator()).to.be.bignumber.equal('200');
-            });
-        });
-        context('when called not by admin', function () {
-            it('revert', async function () {
-                await expectRevert.unspecified(market.setRefererBonus(1, 20, {from : user}));
-            });
-        });
-    });
-
   describe('removeAsset', function () {
     context('if asset is added', function () {
       it('should remove specified asset', async function () {
@@ -148,161 +96,6 @@ describe('BNSDomainNameMarket', function () {
         expect(type).to.be.equal('1');
         await market.removeAsset(tokens.usdt.contract.address, {from : deployer});
         await expectRevert(market.getAsset(tokens.usdt.contract.address), 'Assets.Map: nonexistent key');
-      });
-    });
-  });
-
-  describe('getPrice', function () {
-    let dividendManager;
-    let market
-    let pricing
-    let nft;
-    let usdt;
-    let bnsMarket;
-    let bnsNFT;
-    const share = ether('0.0000000000001');
-
-    beforeEach(async function () {
-      [nft, dividendManager, market, pricing, usdt] = await Promise.all([
-        InvestNFT.new({from: deployer}),
-        DividendManager.new({from: deployer}),
-        InvestNFTMarket.new({from: deployer}),
-        InvestNFTMarketPricePolicy.new({from: deployer}),
-        ERC20Mock.new('USDT Pegged Token', 'USDT', user, ether('2000000'), {from: deployer}),
-
-      ]);
-      await Promise.all([
-        nft.setDividendManager(dividendManager.address, {from: deployer}),
-        nft.grantRole(web3.utils.keccak256('MINTER_ROLE'), market.address, {from: deployer}),
-        dividendManager.setDepositary(nft.address, {from: deployer}),
-        dividendManager.setAsset(usdt.address, 'USDT', 1, {from: deployer}),
-        market.setInvestNFT(nft.address, {from: deployer}),
-        market.setPricePolicy(pricing.address, {from: deployer}),
-        market.setAsset(usdt.address, 'USDT', 1, {from: deployer}),
-        pricing.setPrice(PRICE, {from: deployer})
-      ]);
-      {
-        const [market, names, nft, pricing] = await Promise.all([
-          BNSDomainNameMarket.new({from: deployer}),
-          BNSNamesPolicy.new({from: deployer}),
-          BNSNFT.new({from: deployer}),
-          BNSMarketPricePolicy.new({from: deployer}),
-        ]);
-        bnsMarket = market;
-        bnsNFT = nft;
-        await Promise.all([
-          await market.setBNSNFT(nft.address, {from: deployer}),
-          await market.setDividendManager(dividendManager.address, {from: deployer}),
-          await market.setPricePolicy(pricing.address, {from: deployer}),
-          await market.setNamesPolicy(names.address, {from: deployer}),
-          await market.setAsset(usdt.address, 'USDT', 1, {from: deployer}),
-          await nft.grantRole(web3.utils.keccak256('MINTER_ROLE'), market.address, {from: deployer}),
-          pricing.setPrices(ether(BASE_PRICE_USDT.toString()), SIZES, PRICES_USDT.map(price => ether(price.toString())), {from: deployer})
-        ])
-      }
-    });
-
-    context('if domainName doesn`t exist', function () {
-      it('should return price for specified domainName', async function () {
-        expect (await bnsMarket.getPrice('Hahaha', '', usdt.address)).to.be.bignumber.equal(ether('10000'));
-      });
-    });
-    context('if domainName exists', function () {
-      beforeEach(async () => {
-        await usdt.approve(market.address, PRICE.mul(share), { from: user });
-        await market.buyExactShares(share, usdt.address, { from: user });
-      });
-      it('revert', async function () {
-        await usdt.approve(bnsMarket.address, ether('250000'), {from: user});
-        await bnsMarket.buy('aba', '', usdt.address, {from: user});
-        await expectRevert(bnsMarket.getPrice('aba', '', usdt.address, {from: user}),
-            'Domain name already exists');
-      });
-    });
-  });
-
-  describe('buyWithoutReferer', function () {
-    let dividendManager;
-    let market
-    let pricing
-    let nft;
-    let usdt;
-    let bnsMarket;
-    let bnsNFT;
-    const share = ether('0.0000000000001');
-
-    beforeEach(async function () {
-      [nft, dividendManager, market, pricing, usdt] = await Promise.all([
-        InvestNFT.new({from: deployer}),
-        DividendManager.new({from: deployer}),
-        InvestNFTMarket.new({from: deployer}),
-        InvestNFTMarketPricePolicy.new({from: deployer}),
-        ERC20Mock.new('USDT Pegged Token', 'USDT', user, ether('2000000'), {from: deployer}),
-
-      ]);
-      await Promise.all([
-        nft.setDividendManager(dividendManager.address, {from: deployer}),
-        nft.grantRole(web3.utils.keccak256('MINTER_ROLE'), market.address, {from: deployer}),
-        dividendManager.setDepositary(nft.address, {from: deployer}),
-        dividendManager.setAsset(usdt.address, 'USDT', 1, {from: deployer}),
-        market.setInvestNFT(nft.address, {from: deployer}),
-        market.setPricePolicy(pricing.address, {from: deployer}),
-        market.setAsset(usdt.address, 'USDT', 1, {from: deployer}),
-        pricing.setPrice(PRICE, {from: deployer})
-      ]);
-      {
-        const [market, names, nft, pricing] = await Promise.all([
-          BNSDomainNameMarket.new({from: deployer}),
-          BNSNamesPolicy.new({from: deployer}),
-          BNSNFT.new({from: deployer}),
-          BNSMarketPricePolicy.new({from: deployer}),
-        ]);
-        bnsMarket = market;
-        bnsNFT = nft;
-        await Promise.all([
-          await market.setBNSNFT(nft.address, {from: deployer}),
-          await market.setDividendManager(dividendManager.address, {from: deployer}),
-          await market.setPricePolicy(pricing.address, {from: deployer}),
-          await market.setNamesPolicy(names.address, {from: deployer}),
-          await market.setAsset(usdt.address, 'USDT', 1, {from: deployer}),
-          await nft.grantRole(web3.utils.keccak256('MINTER_ROLE'), market.address, {from: deployer}),
-          pricing.setPrices(ether(BASE_PRICE_USDT.toString()), SIZES, PRICES_USDT.map(price => ether(price.toString())), {from: deployer})
-        ])
-      }
-    });
-
-    context('if domain doesn`t exist', function () {
-      beforeEach(async () => {
-        await usdt.approve(market.address, PRICE.mul(share), { from: user });
-        await market.buyExactShares(share, usdt.address, { from: user });
-      });
-      it('should transfer nft to buyer', async function () {
-        await usdt.approve(bnsMarket.address, ether('250000'), { from: user });
-        const { tx } = await bnsMarket.buyWithoutReferer('ab', usdt.address, { from: user });
-        const [{ args: { from, to, tokenId }}] = await getEvents(tx, nft, 'Transfer', web3);
-        expect(from).to.be.equal(constants.ZERO_ADDRESS);
-        expect(to).to.be.equal(user);
-        expect(tokenId).to.be.bignumber.equal(new BN('0'));
-      });
-      it('should transfer USD to fundraising wallet', async function () {
-        await usdt.approve(bnsMarket.address, ether('250000'), { from: user });
-        const { tx } = await bnsMarket.buyWithoutReferer('ab', usdt.address, { from: user });
-        const [, { args: { from, to, value }}] = await getEvents(tx, usdt, 'Transfer', web3);
-        expect(from).to.be.equal(market.address);
-        expect(to).to.be.equal(token.address);
-        expect(value).to.be.bignumber.equal(ether('250000'));
-      });
-    });
-    context('if domain exists', function () {
-      beforeEach(async () => {
-        await usdt.approve(market.address, PRICE.mul(share), { from: user });
-        await market.buyExactShares(share, usdt.address, { from: user });
-      });
-      it('revert', async function () {
-        await usdt.approve(bnsMarket.address, ether('250000'), {from: user});
-        await bnsMarket.buy('ab', '', usdt.address, {from: user});
-        await expectRevert(bnsMarket.buy('ab', '', usdt.address, {from: user}),
-            'Domain name already exists');
       });
     });
   });
@@ -334,53 +127,43 @@ describe('BNSDomainNameMarket', function () {
         market.setInvestNFT(nft.address, {from: deployer}),
         market.setPricePolicy(pricing.address, {from: deployer}),
         market.setAsset(usdt.address, 'USDT', 1, {from: deployer}),
-        pricing.setPrice(PRICE, {from: deployer})
-      ]);
+        pricing.setPrice(PRICE, {from: deployer}),
+        nft.safeMint(holder1, ether('0.0000000000001'), {from: deployer}),
+      ])
       {
-        const [market, names, nft, pricing] = await Promise.all([
+        const [market, nft] = await Promise.all([
           BNSDomainNameMarket.new({from: deployer}),
-          BNSNamesPolicy.new({from: deployer}),
-          BNSNFT.new({from: deployer}),
-          BNSMarketPricePolicy.new({from: deployer}),
+          BNSNFT.new({from: deployer})
         ]);
         bnsMarket = market;
         bnsNFT = nft;
         await Promise.all([
           await market.setBNSNFT(nft.address, {from: deployer}),
           await market.setDividendManager(dividendManager.address, {from: deployer}),
-          await market.setPricePolicy(pricing.address, {from: deployer}),
-          await market.setNamesPolicy(names.address, {from: deployer}),
           await market.setAsset(usdt.address, 'USDT', 1, {from: deployer}),
           await nft.grantRole(web3.utils.keccak256('MINTER_ROLE'), market.address, {from: deployer}),
-          pricing.setPrices(ether(BASE_PRICE_USDT.toString()), SIZES, PRICES_USDT.map(price => ether(price.toString())), {from: deployer})
+          await market.grantRole(web3.utils.keccak256('MINTER_ROLE'), seller, {from: deployer}),
         ])
       }
     });
-
-    context('if domain doesn`t exist', function () {
-      it('should transfer nft to buyer', async function () {
-        await tokens.usdt.contract.approve(market.address, ether('250000'), { from: user });
-        const { tx } = await market.buy('ab', '', tokens.usdt.contract.address, { from: user });
-        const [{ args: { from, to, tokenId }}] = await getEvents(tx, nft, 'Transfer', web3);
-        expect(from).to.be.equal(constants.ZERO_ADDRESS);
-        expect(to).to.be.equal(user);
-        expect(tokenId).to.be.bignumber.equal(new BN('0'));
-      });
-      it('should transfer USD to fundraising wallet', async function () {
-        await tokens.usdt.contract.approve(market.address, ether('250000'), { from: user });
-        const { tx } = await market.buy('ab', '', tokens.usdt.contract.address, { from: user });
-        const [, { args: { from, to, value }}] = await getEvents(tx, tokens.usdt.contract, 'Transfer', web3);
-        expect(from).to.be.equal(market.address);
-        expect(to).to.be.equal(token.address);
-        expect(value).to.be.bignumber.equal(ether('250000'));
+    context('if has referer', function () {
+      it('should mint nft`s, transfer bonus to referer, distribute dividends', async function () {
+        await usdt.approve(bnsMarket.address, ether('100000'), {from: user});
+        await bnsMarket.buy(domainNames, ether('100000'), user, referer, ether('10000'), usdt.address, {from: seller});
+        expect(await bnsNFT.balanceOf(user)).to.be.bignumber.equal('5');
+        expect(await usdt.balanceOf(referer)).to.be.bignumber.equal(ether('10000'));
+        expect(await dividendManager.withdrawableDividendOf(0, usdt.address)).to.be.bignumber.equal(ether('90000'));
       });
     });
-    context('if domain exists', function () {
-      it('revert', async function () {
-        await tokens.usdt.contract.approve(market.address, ether('250000'), {from: user});
-        await market.buy('ab', '', tokens.usdt.contract.address, {from: user});
-        await expectRevert(market.buy('ab', '', tokens.usdt.contract.address, {from: user}),
-            'Domain name already exists');
+    context('if no referer ', async function () {
+      it('should mint nft`s, distribute dividends', async function () {
+        await usdt.approve(bnsMarket.address, ether('100000'), {from: user});
+        await bnsMarket.buy(domainNames, ether('100000'), user, '0x0000000000000000000000000000000000000000', 0, usdt.address, {from: seller});
+        expect(await bnsNFT.balanceOf(user)).to.be.bignumber.equal('5');
+        expect(await dividendManager.withdrawableDividendOf(0, usdt.address)).to.be.bignumber.equal(ether('100000'));
+        expect(await usdt.balanceOf(holder1)).to.be.bignumber.equal('0');
+        await nft.withdrawDividend({from: holder1});
+        expect(await usdt.balanceOf(holder1)).to.be.bignumber.equal(ether('100000'));
       });
     });
   });

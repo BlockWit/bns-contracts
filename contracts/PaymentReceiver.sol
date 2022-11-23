041666ed0d290ed958df43ef8838b362298f6ae1
Arkadiy A.Davydov <davydov@blockwit.io>
2022-11-23 18:27:48 +0300

    chore(PaymentReceiver): add AssetHandler trait to contract, rename event, add Assets.Key to event

--- contracts/PaymentReceiver.sol
+++ contracts/PaymentReceiver.sol
@@ -5,13 +5,15 @@ pragma solidity ^0.8.14;
 import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "./RecoverableFunds.sol";
+import "./AssetHandler.sol";
 
-contract PaymentReceiver is AccessControl, RecoverableFunds {
+contract PaymentReceiver is AccessControl, AssetHandler, RecoverableFunds {
 
-    event TokenPaid(
+    event PaymentReceived(
         uint256 indexed taskId,
         address indexed buyer,
-        uint256 amount
+        uint256 amount,
+        Assets.Key assetKey
     );
 
     address public recipient;
@@ -25,9 +27,17 @@ contract PaymentReceiver is AccessControl, RecoverableFunds {
         recipient = newRecipient;
     }
 
-    function processPayment(address token, address sender, uint256 amount, uint256 taskId, address buyer) external payable {
-        IERC20(token).transferFrom(sender, recipient, amount);
-        emit TokenPaid(taskId, buyer, amount);
+    function setAsset(Assets.Key key, string memory assetTicker, Assets.AssetType assetType) external onlyRole(DEFAULT_ADMIN_ROLE) returns (bool) {
+        return _setAsset(key, assetTicker, assetType);
+    }
+
+    function processPayment(uint256 taskId, address buyer, uint256 amount, Assets.Key assetKey, address sender) external payable {
+        _transferAssetFrom(sender, recipient, amount, assetKey);
+        emit PaymentReceived(taskId, buyer, amount, assetKey);
+    }
+
+    function removeAsset(Assets.Key key) external onlyRole(DEFAULT_ADMIN_ROLE) returns (bool) {
+        return _removeAsset(key);
     }
 
     function retrieveTokens(address recipient, address tokenAddress) external onlyRole(DEFAULT_ADMIN_ROLE) {
